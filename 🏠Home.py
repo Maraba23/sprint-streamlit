e2c8861968decdd6450c205cbe4f390b8f410ee7
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2023-12-11 12:54:16 -0300

    Update 🏠Home.py

--- 🏠Home.py
+++ 🏠Home.py
@@ -15,7 +15,7 @@ st.title("Página Principal")
 
 
 if not st.session_state["authenticated"] == True:
-    st.warning("Essa pagina contem a documentação do projeto, para acessar ela e qualquer outra pagina você precisa estar logado")
+    st.warning("Essa página contem a documentação do projeto, para acessar ela e qualquer outra página você precisa estar logado")
     st.stop()
 
 st.write("Você está logado!")
@@ -30,7 +30,7 @@ st.markdown(
 
     ## Sobre o projeto
 
-    Nesse projeto tinhamos como objetivo criar uma analise de churn para a Ana Health, uma empresa de planos de saúde.
+    Nesse projeto tínhamos como objetivo criar uma analise de churn para a Ana Health, uma empresa de planos de saúde.
 
     Para isso utilizamos o dataset fornecido pela empresa, e criamos um modelo de machine learning para prever se um cliente iria cancelar o plano de saúde ou não.
 
@@ -38,9 +38,9 @@ st.markdown(
 
     ## Como utilizar o projeto
 
-    ### Pagina de Upload
+    ### Página de Upload
 
-    Na pagina de upload voce pode enviar um arquivo csv para vizualizar graficos e tabelas com os dados do arquivo.
+    Na página de upload você pode enviar um arquivo csv para vizualizar gráficos e tabelas com os dados do arquivo.
 
     exemplo de arquivo csv:
     """
@@ -56,15 +56,15 @@ st.markdown(
     """
     ### Pagina de Predição
 
-    Na pagina de predição voce pode enviar um arquivo csv para o modelo de machine learning prever se um cliente irá cancelar o plano de saúde ou não no proximo mês.
+    Na pagina de predição você pode enviar um arquivo csv para o modelo de machine learning prever se um cliente irá cancelar o plano de saúde ou não no proximo mês.
 
-    Quando o csv é enviado, um preprocessamento dos dados é realizado e mostrado na tela.
+    Quando o csv é enviado, um pré-processamento dos dados é realizado e mostrado na tela.
 
     Após isso, basta usar o botão "Prever Churn" para que o modelo faça a predição.
 
     O arquivo csv pode ter quantos usuarios forem necessarios, mas deve conter as mesmas colunas do exemplo abaixo
 
-    A predicao retorna o id do usuario a ser previsto e a predicao em si. Os resultados podem ser: Continua, Sai da assinatura ou Incerto
+    A predição retorna o id do usuário a ser previsto e a predição em si. Os resultados podem ser: Continua, Sai da assinatura ou Incerto
 
     exemplo de arquivo csv:
     """
@@ -76,7 +76,7 @@ st.dataframe(df)
 
 st.warning(
     """
-    Todas as colunas do arquivo devem estar presentes no arquivo, mesmo que não sejam utilizadas
+    Todas as colunas do arquivo devem estar presentes nele, mesmo que não sejam utilizadas
 
     Sobre a coluna target ("Status"), Ela deve estar presente no dataset, mesmo que esteja totalmente vazia, isso é necessário para o modelo funcionar corretamente
     """
@@ -84,29 +84,29 @@ st.warning(
 
 st.markdown(
     """
-    ### Pagina de registro
+    ### Página de registro
 
-    Na pagina de registro voce pode criar uma conta para acessar as outras paginas do projeto
+    Na página de registro você pode criar uma conta para acessar as outras páginas do projeto
 
-    Para criar a conta eh necessario um `token` fornecido pelo adiministrador do sistema
+    Para criar a conta é necessario um `token` fornecido pelo adiministrador do sistema
 
-    Sem o token, voce não pode criar uma conta
+    Sem o token, você não pode criar uma conta
 
-    ### Pagina de Admin
+    ### Página de Admin
 
-    Na pagina de admin voce pode criar tokens para que outras pessoas possam criar contas
+    Na página de admin você pode criar tokens para que outras pessoas possam criar contas
 
-    Aqui voce pode criar e deletar tokens
+    Aqui você pode criar e deletar tokens
 
-    - Apenas o administrador do sistema pode acessar essa pagina
+    - Apenas o administrador do sistema pode acessar essa página
 
-    ### Pagina de Logs
+    ### Página de Logs
 
-    Na pagina de logs voce pode ver todos os logs de ações realizadas no sistema
+    Na página de logs você pode ver todos os logs de ações realizadas no sistema
 
-    Os logs sao: Upload de arquivos para graficos, Upload de arquivos para predição, Login, Criacao de token, Delecao de token
+    Os logs são: Upload de arquivos para gráficos, Upload de arquivos para predição, Login, Criação de token, Deleção de token
 
-    - Apenas o administrador do sistema pode acessar essa pagina
+    - Apenas o administrador do sistema pode acessar essa página
     """
 )
 
